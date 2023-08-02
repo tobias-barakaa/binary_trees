705c1322fe16385ad6277bd515674639326e874e
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-02 19:04:46 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,21 +11,17 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    // Step 1: Allocate memory for the new node
     binary_tree_t *node_to_create = malloc(sizeof(binary_tree_t));
-    // Step 2: Check if memory allocation was successful
     if (node_to_create == NULL)
     {
         // Return NULL if memory allocation failed
         return (NULL);
     }
 
-    // Step 3: Initialize the new node with the provided value and parent
     node_to_create->n = value;
     node_to_create->parent = parent;
     node_to_create->left = NULL;
     node_to_create->right = NULL;
 
-    // Step 4: Return the pointer to the newly created node
     return (node_to_create);
 }
