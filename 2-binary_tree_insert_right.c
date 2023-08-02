09a647ae2c5af11fea45e42964dc79ba932f37e5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-02 13:56:09 -0700

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,31 +10,31 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node_to_be_created;
+binary_tree_t *node_to_be_created;
+if (parent == NULL)
+{
+return (NULL);
+}
 
-	if (parent == NULL)
-     {
-		return (NULL);
-     }
-	node_to_be_created = malloc(sizeof(binary_tree_t));
-	if (node_to_be_created == NULL)
-     {
-		return (NULL);
-     }
+node_to_be_created = malloc(sizeof(binary_tree_t));
+if (node_to_be_created == NULL)
+{
+return (NULL);
+}
 
-	node_to_be_created->parent = parent;
-	node_to_be_created->n = value;
-	node_to_be_created->left = NULL;
+node_to_be_created->parent = parent;
+node_to_be_created->n = value;
+node_to_be_created->left = NULL;
 
-	if (parent->right)
-	{
-		node_to_be_created->right = parent->right;
-		parent->right = node_to_be_created;
-	}
-	else
-	{
-		parent->right = node_to_be_created;
-		node_to_be_created->right = NULL;
-	}
-	return (node_to_be_created);
+if (parent->right)
+{
+node_to_be_created->right = parent->right;
+parent->right = node_to_be_created;
+}
+else
+{
+parent->right = node_to_be_created;
+node_to_be_created->right = NULL;
+}
+return (node_to_be_created);
 }
