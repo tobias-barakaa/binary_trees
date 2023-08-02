8a90c64153ab585f3dc967f6792d5ad3e45d97b8
Abdia Elema <gababoabdiaelema@gmail.com>
2023-08-02 13:35:04 -0700

    fixed betty errors

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_leaves = counts the leaves of a binary tree
+*binary_tree_leaves - counts the leaves of a binary tree
 *@tree - a  pointer to the root node of the tree to count the number of leaves
 *
 *Return: number of leaves and return 0 if tree is NULL
@@ -9,9 +9,9 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if(tree == NULL)
+	if (tree == NULL)
 		return (0);
-	if(tree->left == NULL && tree->right == NULL)
-		return(1);
-	return(binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
