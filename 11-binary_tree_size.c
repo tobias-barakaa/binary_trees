75ac36cc9309314b56e62feee8e8d0f53ace6aeb
Abdia Elema <gababoabdiaelema@gmail.com>
2023-08-02 13:15:24 -0700

    function that counts the leaves in a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,7 +9,7 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return(0);
 
 	return(binary_tree_size(tree->left) +  binary_tree_size(tree->right) +1);
--- binary_trees.h
+++ binary_trees.h
@@ -29,6 +29,9 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/* binary_tree_print.c */
+void binary_tree_print(const binary_tree_t *tree);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -41,5 +44,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
-
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
 #endif
