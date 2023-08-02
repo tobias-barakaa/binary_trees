bf22ce61963d1d6e091513c1916620b731787a5e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-02 14:44:17 -0700

    3=binary_tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,12 +7,12 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return;
-    }
+if (tree == NULL)
+{
+return;
+}
 
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    free(tree);
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
+free(tree);
 }
