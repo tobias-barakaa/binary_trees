cc773cb9767fb70595bcc78795630b6f176b7d5a
Abdia Elema <gababoabdiaelema@gmail.com>
2023-08-03 00:15:06 -0700

    fixed betty errors

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,21 +9,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t lh_node, rh_node;
+	size_t lh_node, rh_node;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+	return (0);
+	}
 
-    lh_node = binary_tree_height(tree->left) + 1;
-    rh_node = binary_tree_height(tree->right) + 1;
-    if (lh_node > rh_node)
-       return (lh_node);
-    else
-    {
-     return (rh_node);
-    }
+	lh_node = binary_tree_height(tree->left) + 1;
+	rh_node = binary_tree_height(tree->right) + 1;
+	if (lh_node > rh_node)
+		return (lh_node);
+	return (rh_node);
+
 }
 
 /**
