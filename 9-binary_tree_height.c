071a21197428f557809f54b642ccdfc6d514fb6d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-03 03:11:05 -0700

    9-main.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t l_height_node, r_height_node;
 
-	if (tree == NULL)
+	size_t l_height_node, r_height_node;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	l_height_node = binary_tree_height(tree->left);
-	r_height_node = binary_tree_height(tree->right);
-		return ((l_height_node > r_height_node ? l_height_node : r_height_node) + 1);
+	}
+	else
+	{
+		l_height_node = binary_tree_height(tree->left);
+		r_height_node = binary_tree_height(tree->right);
+
+return ((l_height_node > r_height_node ? l_height_node : r_height_node) + 1);
+	}
 }
-
