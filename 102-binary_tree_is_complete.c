14915aab55a517b0b497d9ae6ff011344943ece0
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-03 14:26:59 -0700

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,42 +1,58 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_complete - Check if a binary tree is complete
- * @tree: Pointer to the root node of the tree to check
+ * binary_tree_size - size of binary
  *
- * This function checks whether a binary tree is complete or not. A binary tree
- * is considered complete if all levels are fully filled except possibly the
- * last level, which is filled from left to right.
+ * @tree: tree root
+ * Return: size
+ */
+size_t size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+     {
+		return (0);
+     }
+
+	return (size(tree->left) + size(tree->right) + 1);
+}
+
+/**
+ * comp_tree - determines binary completion
  *
- * Return: 1 if the tree is complete, 0 otherwise
+ * @tree: pointer 
+ * @key: index
+ * @elem_num: element count(number)
+ * Return:Null 
+ */
+int comp_tree(const binary_tree_t *tree, int key, int elem_num)
+{
+	if (tree == NULL)
+     {
+		return (1);
+     }
+
+	if (key >= elem_num)
+     {
+		return (0);
+     }
+
+	return (comp_tree(tree->left, (2 * key) + 1, elem_num) &&
+		comp_tree(tree->right, (2 * key) + 2, elem_num));
+}
+
+
+/**
+ * binary_tree_is_complete - function tree complete
+ *
+ * @tree:  root
+ * Return: Null
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    int flag = 0; // Flag to indicate if a NULL node is encountered
-    binary_tree_t *queue[1024] = {NULL}; // Queue for level-order traversal
-    int front = 0, rear = 0; // Pointers for front and rear of the queue
+	if (tree == NULL)
+     {
+		return (0);
+     }
 
-    if (!tree) // If tree is NULL, it's not complete
-        return (0);
-
-    queue[rear++] = (binary_tree_t *)tree; // Enqueue the root
-
-    while (front < rear)
-    {
-        binary_tree_t *current = queue[front++]; // Dequeue the front node
-
-        if (current)
-        {
-            if (flag)
-                return (0); // If a NULL node was encountered previously, return 0
-
-            queue[rear++] = current->left; // Enqueue left child
-            queue[rear++] = current->right; // Enqueue right child
-        }
-        else
-            flag = 1; // Set flag to indicate a NULL node was encountered
-    }
-
-    return (1); // If no issues, the tree is complete
+	return (comp_tree(tree, 0, size(tree)));
 }
-
