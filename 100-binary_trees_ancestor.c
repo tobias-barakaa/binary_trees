f2a2ce02df765792184f121de04d40fa0abfb24e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-03 10:39:08 -0700

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,29 +2,45 @@
 
 /**
  * binary_trees_ancestor - Find the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
+ * @first: First node
+ * @second: Second node
+ *
+ * This function finds and returns the (LCA) of two nodes
+ * in a binary tree. If no common ancestor is found, it returns NULL.
+ *
  * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
-    const binary_tree_t *ancestor = NULL;
+const binary_tree_t *p, *q;
 
-    if (!first || !second)
-        return NULL;
+if (!first || !second)
+return (NULL);
 
-    while (first)
-    {
-        const binary_tree_t *temp = second;
-        while (temp)
-        {
-            if (temp == first)
-                return (binary_tree_t *)temp;
-            temp = temp->parent;
-        }
-        first = first->parent;
-    }
+/** Store the paths from root to each node**/
+binary_tree_t *path1[1024] = {NULL};
+binary_tree_t *path2[1024] = {NULL};
 
-    return (binary_tree_t *)ancestor;
+size_t len1 = 0, len2 = 0;
+
+/** Traverse from first node to root and store the path**/
+for (p = first; p; p = p->parent)
+path1[len1++] = (binary_tree_t *)p;
+
+/**Traverse from second node to root and store the path**/
+for (q = second; q; q = q->parent)
+path2[len2++] = (binary_tree_t *)q;
+
+/** Find the common ancestor by comparing the paths**/
+binary_tree_t *ancestor = NULL;
+for (size_t i = 0; i < len1 && i < len2; ++i)
+{
+if (path1[len1 - 1 - i] == path2[len2 - 1 - i])
+ancestor = path1[len1 - 1 - i];
+else
+break;
 }
 
+return (ancestor);
+}
