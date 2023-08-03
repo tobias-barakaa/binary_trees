e68db53252a17735b659a916d92c1d48f0fa197f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-03 14:34:28 -0700

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,45 +1,44 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - size of binary
+ * size - size of binary
  *
  * @tree: tree root
  * Return: size
  */
 size_t size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-     {
-		return (0);
-     }
+if (tree == NULL)
+{
+return (0);
+}
 
-	return (size(tree->left) + size(tree->right) + 1);
+return (size(tree->left) + size(tree->right) + 1);
 }
 
 /**
  * comp_tree - determines binary completion
  *
- * @tree: pointer 
+ * @tree: pointer
  * @key: index
  * @elem_num: element count(number)
- * Return:Null 
+ * Return:Null
  */
 int comp_tree(const binary_tree_t *tree, int key, int elem_num)
 {
-	if (tree == NULL)
-     {
-		return (1);
-     }
-
-	if (key >= elem_num)
-     {
-		return (0);
-     }
-
-	return (comp_tree(tree->left, (2 * key) + 1, elem_num) &&
-		comp_tree(tree->right, (2 * key) + 2, elem_num));
+if (tree == NULL)
+{
+return (1);
 }
 
+if (key >= elem_num)
+{
+return (0);
+}
+
+return (comp_tree(tree->left, (2 * key) + 1, elem_num) &&
+comp_tree(tree->right, (2 * key) + 2, elem_num));
+}
 
 /**
  * binary_tree_is_complete - function tree complete
@@ -49,10 +48,10 @@ int comp_tree(const binary_tree_t *tree, int key, int elem_num)
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-     {
-		return (0);
-     }
+if (tree == NULL)
+{
+return (0);
+}
 
-	return (comp_tree(tree, 0, size(tree)));
+return (comp_tree(tree, 0, size(tree)));
 }
